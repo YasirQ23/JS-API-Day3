16c467f82c053772869e24f27e5faa3d8d821d53
Yasir Qureshi <yasirqureshi@Yasirs-MacBook-Air.local>
2022-06-03 09:27:57 -0400

    css changes

--- JScontrolDOM.js
+++ JScontrolDOM.js
@@ -8,11 +8,11 @@ let loadStandings = async (season, round) => {
         let requestedstandings = (data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
         for (let i = 0; i < 7; i++) {
             let driver_row = `<ul class="list-group list-group-horizontal">
-            <li class="list-group-item">${requestedstandings[i].position}</li>
-            <li class="list-group-item">${requestedstandings[i].Driver.givenName} ${requestedstandings[i].Driver.familyName}</li>
-            <li class="list-group-item">${requestedstandings[i].Driver.nationality}</li>
-            <li class="list-group-item">${requestedstandings[i].Constructors[0].constructorId}</li>
-            <li class="list-group-item">${requestedstandings[i].points}</li></ul>`
+            <li class="list-group-item bg-dark">${requestedstandings[i].position}</li>
+            <li class="list-group-item bg-dark">${requestedstandings[i].Driver.givenName} ${requestedstandings[i].Driver.familyName}</li>
+            <li class="list-group-item bg-dark">${requestedstandings[i].Driver.nationality}</li>
+            <li class="list-group-item bg-dark">${requestedstandings[i].Constructors[0].constructorId}</li>
+            <li class="list-group-item bg-dark">${requestedstandings[i].points}</li></ul>`
             document.getElementById('contentr').insertAdjacentHTML('beforeend', driver_row);
         }
     }
